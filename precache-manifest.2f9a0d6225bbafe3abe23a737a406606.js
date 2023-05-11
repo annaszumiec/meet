0ce7b9c731fc80048414302294dffde6ca7a5377
annamaria <szumiecam@gmail.com>
2023-05-11 12:21:59 +0200

    Updates

---
+++ precache-manifest.2f9a0d6225bbafe3abe23a737a406606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3876ac5855a16c44dc5",
+    "url": "/meet/static/css/main.1a57aaee.chunk.css"
+  },
+  {
+    "revision": "f3876ac5855a16c44dc5",
+    "url": "/meet/static/js/main.f3876ac5.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/meet/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "4ec1121691fcda9e7355",
+    "url": "/meet/static/js/2.4ec11216.chunk.js"
+  },
+  {
+    "revision": "9b2c2eae3a9eaad59703",
+    "url": "/meet/static/js/runtime~main.9b2c2eae.js"
+  },
+  {
+    "revision": "e67fce8bf753fcd43d5f4c16c0f9bd90",
+    "url": "/meet/index.html"
+  }
+];
