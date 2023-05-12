7c850f1fc21513faae689db84d91dbf30d833014
annamaria <szumiecam@gmail.com>
2023-05-12 12:58:12 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import EventList from './EventList';
-import CitySearch from './CitySearch';
-import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api';
+import React, { Component } from "react";
+import "./App.css";
+import EventList from "./EventList";
+import CitySearch from "./CitySearch";
+import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
 // import './nprogress.css';
-
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
-  }
+  };
 
   componentDidMount() {
     this.mounted = true;
@@ -26,33 +25,30 @@ class App extends Component {
     this.mounted = false;
   }
 
-
   updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
       this.setState({
-        events: locationEvents
+        events: locationEvents,
       });
     });
-  }
+  };
 
   render() {
     return (
       <div className="App">
-
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
         <NumberOfEvents events={this.state.events} />
         <EventList events={this.state.events} />
-
-
       </div>
     );
   }
 }
-
-
-
 
 export default App;
